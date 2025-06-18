1ff1b8d8fc91b9b158561be9c90156b537336dd2
Max Kamenskii <80255339+MaxKamenskii@users.noreply.github.com>
2025-06-18 08:06:41 +0500

    Add setPriority function for ToDos

--- createToDo.js
+++ createToDo.js
@@ -2,16 +2,16 @@ console.log("Module loaded");
 export const allToDos = [];
 
 export class ToDo {
-    constructor(title, description, dueDate, priority, list) {
+    constructor(title, description, dueDate, priority, list, done) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
-        this.list = list
+        this.list = list;
     }
 }
 
-export function addToTheList(title, description, dueDate, priority) {
+export function addToAllToDos(title, description, dueDate, priority) {
     const toDo = new ToDo(title, description, dueDate, priority)
     allToDos.push(toDo)
 }
@@ -21,4 +21,16 @@ export function deleteToDo(title) {
     if (indexOfToDo !== -1) {
         allToDos.splice(indexOfToDo, 1)
     }
+}
+
+export function addToTheList(toDoTitle, listName) {
+    let indexOfToDo = allToDos.findIndex(todo => todo.title === toDoTitle)
+    allToDos[indexOfToDo].list = listName.name;
+    listName.ToDos.push(allToDos[indexOfToDo]);
+}
+
+export function setPriority(toDoTitle, priority) {
+    let indexOfToDo = allToDos.findIndex(todo => todo.title === toDoTitle)
+    allToDos[indexOfToDo].priority = priority
+    console.log(allToDos[indexOfToDo].priority)
 }
--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import {allToDos} from './createToDo.js'
-import {ToDo, addToTheList, deleteToDo} from './createToDo.js'
-addToTheList("finish project", "finish the todo list", "June 23rd", "high")
-addToTheList("ebay", "sell the monitor", "June 17", "low")
+import {ToDo, addToAllToDos, deleteToDo, addToTheList, setPriority} from './createToDo.js'
+import {List} from './projects.js'
+addToAllToDos("finish project", "finish the todo list", "June 23rd", "high")
+addToAllToDos("ebay", "sell the monitor", "June 17", "low")
 
 console.log("After adding:", [...allToDos]);
 
-deleteToDo("finish project")
+// deleteToDo("finish project")
 
 console.log("After deleting:", [...allToDos]);
+
+const homeList = new List("Home")
+const studyList = new List("study")
+
+addToTheList("ebay", homeList)
+addToTheList("finish project", homeList)
+
+
+console.log(homeList.ToDos)
+console.log(studyList.ToDos)
+console.log("homeList todos",homeList.ToDos)
+
+
+
+console.log(allToDos[1])
